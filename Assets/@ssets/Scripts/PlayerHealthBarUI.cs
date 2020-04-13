4b76882e7ccf6301d639cb8ae965cdfe5b91bac8
Anshori <anshori.touchten@gmail.com>
2020-04-13 20:01:43 +0700

    + Added Player Health Bar UI

---
+++ Assets/@ssets/Scripts/PlayerHealthBarUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBarUI : MonoBehaviour
+{
+    [SerializeField] private Slider healthBarSlider;
+
+    private float maxHealth;
+    private float currentHealth;
+
+    private void Start()
+    {
+        InitiateHealthBar();
+    }
+
+    private void InitiateHealthBar()
+    {
+        healthBarSlider.maxValue = maxHealth;
+        healthBarSlider.value = maxHealth;
+    }
+
+    public void DecreaseHealth(float damageAmount)
+    {
+        healthBarSlider.value -= damageAmount;
+    }
+
+    public void IncreaseHealth(float healAmount)
+    {
+        healthBarSlider.value += healAmount;
+    }
+}
