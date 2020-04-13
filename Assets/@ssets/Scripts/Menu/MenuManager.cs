f6e91f59283decd796dc75f2fc5d9e6c6755b797
Anshori <anshori.touchten@gmail.com>
2020-04-13 18:49:30 +0700

    + Added Title Screen

---
+++ Assets/@ssets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void GoToSceneSingle(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void GoToSceneAdditive(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/@ssets/Scripts/Menu/TitleScreenAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenAnimation : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void InitiateButton()
+    {
+
+    }
+
+
+}
